28dcf818d1381d891f545f811b8e6f0418b97153
OvershotHavik <plavetzky@gmail.com>
2022-01-07 16:29:37 -0500

    Initial Commit

---
+++ Combine Examples/Combine_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_ExamplesApp.swift
+//  Combine Examples
+//
+//  Created by Steve Plavetzky on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Combine_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
