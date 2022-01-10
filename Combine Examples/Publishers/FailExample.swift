0d7ee365294091c7bc7ebce95e174f63f54138b2
OvershotHavik <plavetzky@gmail.com>
2022-01-10 16:01:10 -0500

    Testing out tabbed results
    Testing out tabbed results

---
+++ Combine Examples/Publishers/FailExample.swift
@@ -0,0 +1,26 @@
+//
+//  FailExample.swift
+//  Combine Examples
+//
+//  Created by Steve Plavetzky on 1/8/22.
+//
+
+import SwiftUI
+import Combine
+
+class FailExampleVM: ObservableObject{
+    
+}
+
+
+struct FailExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FailExample_Previews: PreviewProvider {
+    static var previews: some View {
+        FailExample()
+    }
+}
